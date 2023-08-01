9de6a6cac17771a951159788287abb0f484f6e8b
Saulius Gražulis <grazulis@ibt.lt>
2023-08-01 16:05:47 +0000

    Adam_Jasinski/ (saulius@kukutis) Checking the number of provided arguments to avoid unhandled exceptions.
    git-svn-id: svn://saulius.grazulis.lt/galve-lafs/trunk@4 5a20f403-c72f-4365-b5ef-363bffb47aca

--- ada_tahoe/src/ada_tahoe.adb
+++ ada_tahoe/src/ada_tahoe.adb
@@ -4,6 +4,12 @@ with Tahoe; use Tahoe;
 
 procedure Ada_Tahoe is
 begin
-   Put_Line (Storage_Index_From_File_Key (Argument (1)));
+   if Argument_Count > 0 then
+      Put_Line (Storage_Index_From_File_Key (Argument (1)));
+   else
+      Put_Line (Command_Name & ": " & 
+                  "Please provide a Tahoe-LAFS third " &
+                  "capability component (read key) as an argument");
+   end if;
    return;
 end Ada_Tahoe;
