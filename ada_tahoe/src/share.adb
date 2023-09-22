eca88975d162f8b1cce519cd9143db77e88e4cf8
Adam Jasinski <adam@jasinski.lt>
2023-09-22 12:58:28 +0000

    overwrite Read attribute for bigendian values
    git-svn-id: svn://saulius.grazulis.lt/galve-lafs/trunk@10 5a20f403-c72f-4365-b5ef-363bffb47aca

--- ada_tahoe/src/share.adb
+++ ada_tahoe/src/share.adb
@@ -9,7 +9,7 @@ package body Share is
       --  Ceiling function
       Block_Size : constant Positive :=
         (Segment_Size + (Required_Shares - 1)) / Required_Shares;
-      type Block is array (Integer range 0 .. Block_Size) of Byte;
+      type Block is array (Integer range 0 .. Block_Size) of Word;
 
       S               : Stream_Access;
       Share_File      : File_Type;
@@ -19,8 +19,8 @@ package body Share is
       Open (Share_File, In_File, "../go-tahoe/3");
       S := Stream (Share_File);
       Share_Header'Read (S, My_Share_Header);
-      My_Share_Header.Block_Size := Unsigned_32 (Block_Size);
-      Display_Share_Content (My_Share_Header);
+      My_Share_Header.Block_Size := Word (Block_Size);
+      Display_Share_Header (My_Share_Header);
       Block'Read (S, My_Block);
 
       Close (Share_File);
@@ -29,56 +29,59 @@ package body Share is
       --  Now My_Share contains the values read from the binary file
    end Read_Share;
 
-   procedure Display_Share_Content (My_Share_Header : Share_Header) is
+   procedure Read_Big_Endian_Word
+     (Stream : access Ada.Streams.Root_Stream_Type'Class; Item : out Word'Base)
+   is
+      B1, B2, B3, B4 : Byte;
+   begin
+      Byte'Read (Stream, B1);
+      Byte'Read (Stream, B2);
+      Byte'Read (Stream, B3);
+      Byte'Read (Stream, B4);
+
+      Item :=
+        Shift_Left (Word (B1), 24) or Shift_Left (Word (B2), 16) or
+        Shift_Left (Word (B3), 8) or Word (B4);
+
+   end Read_Big_Endian_Word;
+
+   procedure Display_Share_Header (My_Share_Header : Share_Header) is
    begin
       Ada.Text_IO.Put_Line
-        ("Share version: " &
-         Interfaces.Unsigned_32'Image (My_Share_Header.Version));
+        ("Share version: " & Word'Image (My_Share_Header.Version));
       Ada.Text_IO.Put_Line
-        ("Share Data Length: " &
-         Interfaces.Unsigned_32'Image (My_Share_Header.Data_Length));
+        ("Share Data Length: " & Word'Image (My_Share_Header.Data_Length));
       Ada.Text_IO.Put_Line
-        ("Lease Number: " &
-         Interfaces.Unsigned_32'Image (My_Share_Header.Lease_number));
+        ("Lease Number: " & Word'Image (My_Share_Header.Lease_number));
       Ada.Text_IO.Put_Line
-        ("Share version: " &
-         Interfaces.Unsigned_32'Image (My_Share_Header.Version));
+        ("Share version: " & Word'Image (My_Share_Header.Version));
       Ada.Text_IO.Put_Line
-        ("Block Size: " &
-         Interfaces.Unsigned_32'Image (My_Share_Header.Block_Size));
+        ("Block Size: " & Word'Image (My_Share_Header.Block_Size));
       Ada.Text_IO.Put_Line
-        ("Data Size: " &
-         Interfaces.Unsigned_32'Image (My_Share_Header.Data_Size));
+        ("Data Size: " & Word'Image (My_Share_Header.Data_Size));
       Ada.Text_IO.Put_Line
-        ("Data offset: " &
-         Interfaces.Unsigned_32'Image (My_Share_Header.Data_Offset));
+        ("Data offset: " & Word'Image (My_Share_Header.Data_Offset));
       Ada.Text_IO.Put_Line
         ("Plaintext hash tree offset: " &
-         Interfaces.Unsigned_32'Image
-           (My_Share_Header.Plaintext_Hash_Tree_Offset));
+         Word'Image (My_Share_Header.Plaintext_Hash_Tree_Offset));
       Ada.Text_IO.Put_Line
         ("Crypttext hash tree offset: " &
-         Interfaces.Unsigned_32'Image
-           (My_Share_Header.Crypttext_Hash_Tree_Offset));
+         Word'Image (My_Share_Header.Crypttext_Hash_Tree_Offset));
       Ada.Text_IO.Put_Line
         ("Block hashes offset: " &
-         Interfaces.Unsigned_32'Image (My_Share_Header.Block_Hashes_Offset));
+         Word'Image (My_Share_Header.Block_Hashes_Offset));
       Ada.Text_IO.Put_Line
         ("Share hashes offset: " &
-         Interfaces.Unsigned_32'Image (My_Share_Header.Share_Hashes_Offset));
+         Word'Image (My_Share_Header.Share_Hashes_Offset));
       Ada.Text_IO.Put_Line
         ("URI Extension Length and URI Extension block offset: " &
-         Interfaces.Unsigned_32'Image (My_Share_Header.URI_Extension_Offset));
-      Ada.Text_IO.Put_Line
-        ("Test" &
-         Interfaces.Unsigned_32'Image
-           ((My_Share_Header.Data_Size) / My_Share_Header.Block_Size));
-   end Display_Share_Content;
+         Word'Image (My_Share_Header.URI_Extension_Offset));
+   end Display_Share_Header;
 
    procedure Read_Blocks
      (My_Share_Header : Share_Header; Share_File : File_Type)
    is
-      Total_Blocks : Interfaces.Unsigned_32 := My_Share_Header.Data_Size;
+      Total_Blocks : Word := My_Share_Header.Data_Size;
    begin
       Ada.Text_IO.Put ("");
 
