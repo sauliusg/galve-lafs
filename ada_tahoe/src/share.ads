86cd33bdd2580723b560962b2318c5b86e505ce6
Adam_Jasinski <Adam_Jasinski@5a20f403-c72f-4365-b5ef-363bffb47aca>
2023-09-22 12:58:28 +0000

    overwrite Read attribute for bigendian values
    git-svn-id: svn://saulius.grazulis.lt/galve-lafs/trunk@10 5a20f403-c72f-4365-b5ef-363bffb47aca

--- ada_tahoe/src/share.ads
+++ ada_tahoe/src/share.ads
@@ -4,29 +4,34 @@ with Ada.Streams.Stream_IO;
 
 package Share is
 
+   -- An Unsigned_32 with big-endian encoding in input streams.
+   type Word is new Interfaces.Unsigned_32;
    type Byte is new Interfaces.Unsigned_8;
-   type Kilobyte is array (Integer range 0 .. 1_023) of Byte;
-   type Kilobyte_array is array (Integer range <>) of Kilobyte;
 
    type Share_Header is record
-      Version                    : Unsigned_32;
-      Data_Length                : Unsigned_32;
-      Lease_number               : Unsigned_32;
+      Version                    : Word;
+      Data_Length                : Word;
+      Lease_number               : Word;
       --  unused as it can be calculated from the URI
-      Version_Junk               : Unsigned_32;
-      Block_Size                 : Unsigned_32;
-      Data_Size                  : Unsigned_32;
-      Data_Offset                : Unsigned_32;
-      Plaintext_Hash_Tree_Offset : Unsigned_32;
-      Crypttext_Hash_Tree_Offset : Unsigned_32;
-      Block_Hashes_Offset        : Unsigned_32;
-      Share_Hashes_Offset        : Unsigned_32;
-      URI_Extension_Offset       : Unsigned_32;
+      Version_Junk               : Word;
+      Block_Size                 : Word;
+      Data_Size                  : Word;
+      Data_Offset                : Word;
+      Plaintext_Hash_Tree_Offset : Word;
+      Crypttext_Hash_Tree_Offset : Word;
+      Block_Hashes_Offset        : Word;
+      Share_Hashes_Offset        : Word;
+      URI_Extension_Offset       : Word;
    end record;
 
+   procedure Read_Big_Endian_Word
+     (Stream :     access Ada.Streams.Root_Stream_Type'Class;
+      Item   : out Word'Base);
    procedure Read_Share (Segment_Size : Positive; Required_Shares : Positive);
-   procedure Display_Share_Content (My_Share_Header : Share_Header);
+   procedure Display_Share_Header (My_Share_Header : Share_Header);
    procedure Read_Blocks
      (My_Share_Header : Share_Header;
       Share_File      : Ada.Streams.Stream_IO.File_Type);
+
+   for Word'Read use Read_Big_Endian_Word;
 end Share;
