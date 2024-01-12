7eb66f714b4313865049e97c283af91ce459e035
Adam Jasinski <adam@jasinski.lt>
2024-01-12 08:35:46 +0000

    add test data
    git-svn-id: svn://saulius.grazulis.lt/galve-lafs/trunk@46 5a20f403-c72f-4365-b5ef-363bffb47aca

---
+++ data/lorem/block.py
@@ -0,0 +1,21 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python script.py <input_file_path>")
+    sys.exit(1)
+
+input_file_path = sys.argv[1]
+output_file_path = input_file_path + "_block"
+
+start_byte = 36  # 0-based index, so 37th byte is at index 36
+end_byte = 12502  # 0-based index, so 12467th byte is at index 12466
+
+with open(input_file_path, 'rb') as input_file:
+    input_file.seek(start_byte)
+    fragment_data = input_file.read(end_byte - start_byte + 1)
+
+with open(output_file_path, 'wb') as output_file:
+    output_file.write(fragment_data)
+
+print(f"Fragment extracted from {input_file_path} and saved to {output_file_path}")
+
