3032c275ef5ee067529cf66269dff9a84509a166
Adam_Jasinski <Adam_Jasinski@5a20f403-c72f-4365-b5ef-363bffb47aca>
2024-03-12 13:09:55 +0000

    More work towards multi-block decoding, fiddle with profiling
    git-svn-id: svn://saulius.grazulis.lt/galve-lafs/trunk@64 5a20f403-c72f-4365-b5ef-363bffb47aca

--- ada_tahoe/src/decoder.adb
+++ ada_tahoe/src/decoder.adb
@@ -8,27 +8,15 @@ with Interfaces.C; use Interfaces.C;
 package body Decoder is
    procedure Decode_File (File_URI : URI; Share_Names : Share_Name_Array) is
       use Interfaces;
-      Shares : Share_Access_Array (1 .. Share_Names'Length);
+      Shares           : Share_Access_Array (1 .. Share_Names'Length);
+      Primary_Blocks_N : Natural := 0;
 
-      type Share_Number_Array is
-        array (1 .. Share_Names'Length) of aliased Integer;
-
-      Share_Numbers : Share_Number_Array;
+      Share_Numbers : Share_Number_Array (1 .. Share_Names'Last);
 
       Decoder : constant access fec_t :=
         fec_new
           (Interfaces.C.unsigned_short (File_URI.Needed_Shares),
            Interfaces.C.unsigned_short (File_URI.Total_Shares));
-
-      Primary_Blocks            : Integer := 0;
-      Decoding_Blocks           :
-        Block_Access_Array (1 .. Integer (File_URI.Needed_Shares));
-      Decoding_Blocks_Addresses :
-        Block_Address_Array (1 .. Integer (File_URI.Needed_Shares));
-      Result_Blocks             :
-        Block_Access_Array (1 .. Integer (File_URI.Needed_Shares));
-      Result_Block_Addresses    :
-        Block_Address_Array (1 .. Integer (File_URI.Needed_Shares));
    begin
       for Index in Share_Names'Range loop
          Shares (Index) := Read_Share (To_String (Share_Names (Index)));
@@ -41,96 +29,92 @@ package body Decoder is
       for Index in Shares'Range loop
          Share_Numbers (Index) := Shares (Index).Share_Number;
          if Share_Numbers (Index) < Integer (File_URI.Needed_Shares) then
-            Primary_Blocks := Primary_Blocks + 1;
+            Primary_Blocks_N := Primary_Blocks_N + 1;
          end if;
       end loop;
 
       for I in 1 .. Integer (Shares (1).URI_Extension_Block.Segment_Count - 1)
       loop
-         for J in 1 .. Integer (File_URI.Needed_Shares) loop
-            Decoding_Blocks (J) := Next_Block (Shares (J));
-            Result_Blocks (J)   := new Block (1 .. Decoding_Blocks (J)'Length);
-         end loop;
-         Decoding_Blocks_Addresses :=
-           Convert_To_Address_Array (Decoding_Blocks);
-         Result_Block_Addresses    := Convert_To_Address_Array (Result_Blocks);
-         fec_decode
-           (Decoder, Decoding_Blocks_Addresses'Address,
-            Result_Block_Addresses'Address,
-            Share_Numbers (Share_Numbers'First)'Access,
-            size_t
-              (Shares (1).URI_Extension_Block.Codec_Params.Segment_Size /
-               Interfaces.Unsigned_64
-                 (Shares (1).URI_Extension_Block.Codec_Params.Needed_Shares)));
-         Result_Blocks (1) := Decoding_Blocks (1);
-         Result_Blocks (2) := Decoding_Blocks (2);
-         Result_Blocks (3) := Decoding_Blocks (3);
-
-         declare
-            use Byte_IO;
-            F         : Byte_IO.File_Type;
-            File_Name : constant String := "output.dat";
-            Temp      : Word            := 0;
-            Padding_N : Natural         :=
-              Natural
-                ((Result_Blocks (1).all'Length * 4) mod
-                 Shares (1).Data_Header.Block_Size);
-         begin
-            Byte_IO.Open (F, Byte_IO.Append_File, File_Name);
-            Write_Block (F, Result_Blocks (1), Padding => Padding_N);
-            Write_Block (F, Result_Blocks (2), Padding => Padding_N);
-            Write_Block (F, Result_Blocks (3), Padding => 0);
-            Byte_IO.Close (F);
-         end;
+         Decode_Block
+           (Decoder          => Decoder, Shares => Shares,
+            Share_Numbers    => Share_Numbers,
+            Primary_Blocks_N => Primary_Blocks_N, Last => False,
+            Needed_Shares    => Shares (1).URI_Extension_Block.Needed_Shares);
       end loop;
-      for J in 1 .. Integer (File_URI.Needed_Shares) loop
+      Decode_Block
+        (Decoder => Decoder, Shares => Shares, Share_Numbers => Share_Numbers,
+         Primary_Blocks_N => Primary_Blocks_N, Last => True,
+         Needed_Shares    => Shares (1).URI_Extension_Block.Needed_Shares);
+   end Decode_File;
+
+   procedure Decode_Block
+     (Decoder       :        access fec_t; Shares : in out Share_Access_Array;
+      Share_Numbers : in out Share_Number_Array; Primary_Blocks_N : Natural;
+      Last          :        Boolean; Needed_Shares : Share_Count)
+   is
+      use Interfaces;
+      Block_Size                : Unsigned_64;
+      Decoding_Blocks : Block_Access_Array (1 .. Integer (Needed_Shares));
+      Result_Blocks : Block_Access_Array (1 .. Integer (Needed_Shares));
+      Decoding_Blocks_Addresses :
+        Block_Address_Array (1 .. Integer (Needed_Shares));
+      Result_Block_Addresses    :
+        Block_Address_Array (1 .. Integer (Needed_Shares));
+   begin
+      for J in 1 .. Integer (Needed_Shares) loop
          Decoding_Blocks (J) := Next_Block (Shares (J));
          Result_Blocks (J)   := new Block (1 .. Decoding_Blocks (J)'Length);
       end loop;
       Decoding_Blocks_Addresses := Convert_To_Address_Array (Decoding_Blocks);
       Result_Block_Addresses    := Convert_To_Address_Array (Result_Blocks);
+      if not Last then
+         Block_Size :=
+           (Shares (1).URI_Extension_Block.Codec_Params.Segment_Size /
+            Interfaces.Unsigned_64 (Needed_Shares));
+      else
+         Block_Size :=
+           Shares (1).URI_Extension_Block.Tail_Codec_Params.Segment_Size /
+           Interfaces.Unsigned_64 (Needed_Shares);
+      end if;
       fec_decode
         (Decoder, Decoding_Blocks_Addresses'Address,
          Result_Block_Addresses'Address,
-         Share_Numbers (Share_Numbers'First)'Access,
-         size_t
-           (Shares (1).URI_Extension_Block.Tail_Codec_Params.Segment_Size +
-            2 /
-              Interfaces.Unsigned_64
-                (Shares (1).URI_Extension_Block.Tail_Codec_Params
-                   .Needed_Shares)));
-      Result_Blocks (1) := Decoding_Blocks (1);
-      Result_Blocks (2) := Decoding_Blocks (2);
-      Result_Blocks (3) := Decoding_Blocks (3);
-      --  for Index in 1 .. Primary_Blocks loop
-      --     Result_Blocks (Index + 1) := Result_Blocks (Index);
-      --     Result_Blocks (Index)     := Decoding_Blocks (Index);
-      --  end loop;
+         Share_Numbers (Share_Numbers'First)'Access, Block_Size);
+      if Primary_Blocks_N = Natural (Needed_Shares) then
+         Result_Blocks := Decoding_Blocks;
+      else
+         for Index in 1 .. Primary_Blocks_N loop
+            Result_Blocks (Index + 1) := Result_Blocks (Index);
+            Result_Blocks (Index)     := Decoding_Blocks (Index);
+         end loop;
+      end if;
 
       declare
          use Byte_IO;
          F         : Byte_IO.File_Type;
          File_Name : constant String := "output.dat";
-         Temp      : Word            := 0;
-         Padding_N : Natural         :=
-           Natural
-             ((Result_Blocks (1).all'Length * 4) mod
-              Shares (1).Last_Block.all'Length);
+         Padding_N : Natural;
       begin
+         if not Last then
+            Padding_N :=
+              Natural
+                ((Unsigned_64 (Result_Blocks (1).all'Length) * 4) -
+                 (Shares (1).URI_Extension_Block.Segment_Size /
+                  Unsigned_64 (Needed_Shares)));
+         else
+            Padding_N :=
+              Natural
+                ((Unsigned_64 (Result_Blocks (1).all'Length) * 4) -
+                 Shares (1).URI_Extension_Block.Tail_Codec_Params
+                     .Segment_Size /
+                   Unsigned_64 (Needed_Shares));
+         end if;
          Byte_IO.Open (F, Byte_IO.Append_File, File_Name);
-         Write_Block (F, Result_Blocks (1), Padding => Padding_N);
-         Write_Block (F, Result_Blocks (2), Padding => Padding_N);
-         Write_Block (F, Result_Blocks (3), Padding => Padding_N);
-         --  for Block of Result_Blocks loop
-         --     if Padding_N /= 0 then
-         --        Padding_N := Padding_N - 1;
-         --        Write_Block (F, Block, Padding => True);
-         --     else
-         --        Write_Block (F, Block, Padding => True);
-         --     end if;
-         --  end loop;
+         for I in 1 .. Result_Blocks'Last - 1 loop
+            Write_Block (F, Result_Blocks (I), Padding => Padding_N);
+         end loop;
+         Write_Block (F, Result_Blocks (Result_Blocks'Last), Padding => 0);
          Byte_IO.Close (F);
       end;
-
-   end Decode_File;
+   end Decode_Block;
 end Decoder;
--- ada_tahoe/src/decoder.ads
+++ ada_tahoe/src/decoder.ads
@@ -1,7 +1,17 @@
 with Uri_Read;              use Uri_Read;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Share;
+with Types;
+with fec_h;
 
 package Decoder is
    type Share_Name_Array is array (Natural range <>) of Unbounded_String;
+   type Share_Number_Array is array (Natural range <>) of aliased Integer;
+
    procedure Decode_File (File_URI : URI; Share_Names : Share_Name_Array);
+
+   procedure Decode_Block
+     (Decoder : access fec_h.fec_t; Shares : in out Share.Share_Access_Array;
+      Share_Numbers : in out Share_Number_Array; Primary_Blocks_N : Natural;
+      Last          :        Boolean; Needed_Shares : Types.Share_Count);
 end Decoder;
