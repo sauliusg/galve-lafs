2c6fb083d44ab5dce31aff015ce435d9d8463572
Adam Jasinski <adam@jasinski.lt>
2024-04-02 12:49:57 +0000

    add openssl bindings
    git-svn-id: svn://saulius.grazulis.lt/galve-lafs/trunk@77 5a20f403-c72f-4365-b5ef-363bffb47aca

--- ada_aes/src/ada_aes.adb
+++ ada_aes/src/ada_aes.adb
@@ -1,8 +1,11 @@
-with EVP;          use EVP;
-with Types;        use Types;
+with EVP;                   use EVP;
+with Types;                 use Types;
 with Text_IO;
 with System;
-with Interfaces.C; use Interfaces.C;
+with Interfaces.C;          use Interfaces.C;
+with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
+with Ada.Streams;           use Ada.Streams;
+with Ada.Command_Line;      use Ada.Command_Line;
 
 procedure Ada_Aes is
    ctx       : EVP_CIPHER_CTX_PTR;
@@ -14,14 +17,80 @@ procedure Ada_Aes is
    engine_st : ENGINE_ST_PTR;
    Key       : aliased char_array := To_C ("abcdefghabcdefgh");
    IV        : aliased char_array := To_C ("abcdefghabcdefgh");
+
+   InputF        : File_Type;
+   OutputF       : File_Type;
+   InputS        : Stream_Access;
+   OutputS       : Stream_Access;
+   Inlen, Outlen : Integer;
+
+   Mode : String := Argument (1); -- Operation ('encrypt' or 'decrypt')
+   Input_File_Name  : String := Argument (2);
+   Output_File_Name : String := Argument (3);
+
+   Test       : Byte;
+   Inbuf      : Byte_Array (1 .. BUFSIZE)      := (others => 0);
+   Outbuf     : Byte_Array (1 .. BUFSIZE + 32) := (others => 0);
+   Outbuf_Len : aliased Integer;
+   Inbuf_Len  : aliased Integer                := BUFSIZE;
+
+   Op : Operation;
 begin
-   -- EVP_CipherFetch (Ctx => ossl_ctx, Algorithm => Algo, Properties => Prop);
+   if Mode = "encrypt" then
+      Op := Encrypt;
+   elsif Mode = "decrypt" then
+      Op := Decrypt;
+   else
+      Text_IO.Put_Line ("Invalid mode. Use 'encrypt' or 'decrypt'");
+      return;
+   end if;
+
+   Open (InputF, In_File, Input_File_Name);
+   Open (OutputF, Out_File, Output_File_Name);
+   InputS  := Stream (InputF);
+   OutputS := Stream (OutputF);
+
    ctx    := EVP_CIPHER_CTX_new;
    cipher := EVP_aes_128_ctr;
-   Junk   :=
-     EVP_CipherInit
+   if EVP_CipherInit
        (Ctx      => ctx, Cipher => cipher, CipherKey => Key'Access,
-        CipherIV => IV'Access, Op => 1);
+        CipherIV => IV'Access, Op => Op) /=
+     1
+   then
+      raise Constraint_Error;
+   end if;
+   Text_IO.Put_Line (Size (InputF)'Image);
+   Text_IO.Put_Line (Index (InputF)'Image);
+
+   while (Index (InputF) < Size (InputF)) loop
+      Byte_Array'Read (InputS, Inbuf);
+      if EVP_CipherUpdate
+          (ctx, Outbuf (1)'Access, Outbuf_Len'Access, Inbuf (1)'Access,
+           Inbuf_Len) /=
+        1
+      then
+         raise Constraint_Error;
+      end if;
+      Byte_Array'Write (OutputS, Outbuf (1 .. Outbuf_Len));
+   end loop;
+   if EVP_CipherFinal_ex (ctx, Outbuf (1)'Access, Outbuf_Len'Access) /= 1 then
+      raise Constraint_Error;
+   end if;
+   Byte_Array'Write (OutputS, Outbuf (1 .. Outbuf_Len));
 
    EVP_CIPHER_CTX_free (ctx);
+
+   Close (InputF);
+   Close (OutputF);
+exception
+   when End_Error =>
+      if EVP_CipherFinal_ex (ctx, Outbuf (1)'Access, Outbuf_Len'Access) /= 1
+      then
+         raise Constraint_Error;
+      end if;
+      Byte_Array'Write (OutputS, Outbuf (1 .. Outbuf_Len));
+      Text_IO.Put_Line (Outbuf_Len'Image);
+      Text_IO.Put_Line (Outbuf_Len'Image);
+
+      EVP_CIPHER_CTX_free (ctx);
 end Ada_Aes;
--- ada_aes/src/external/openssl/types.ads
+++
@@ -1,5 +0,0 @@
-with Interfaces.C; use Interfaces.C;
-
-package Types is
-   type Operation is new Integer range 0 .. 1;
-end Types;
